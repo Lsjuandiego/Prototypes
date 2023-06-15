a4d05fba197a805be9a18af1a4d3e65e661dfb34
singbio <lsjuandiegoo@gmail.com>
2023-06-14 22:59:30 -0500

    logo

--- src/headerAndFooter/headerFooter.js
+++ src/headerAndFooter/headerFooter.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../images/logoUcaldas.png"; // Ruta de la imagen
 
 const Header = () => {
   return (
-    <header className="bg-blue-300">
-      <nav className="container mx-auto py-4">
+    <header className="bg-blue-500 flex justify-between items-center py-4">
+      <nav className="container mx-auto">
         <ul className="flex space-x-4 text-white font-sans">
           <li>
             <Link
@@ -17,13 +18,14 @@ const Header = () => {
           {/* Agrega aquí más elementos <li> para otros links en el header */}
         </ul>
       </nav>
+      <img src={logo} alt="Logo Ucaldas" className="w-12 h-12" />
     </header>
   );
 };
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-300 text-white text-center py-4 fixed bottom-0 left-0 w-full">
+    <footer className="bg-blue-500 text-white text-center py-4 fixed bottom-0 left-0 w-full">
       {/* Agrega aquí el contenido del footer */}
     </footer>
   );
